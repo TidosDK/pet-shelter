576aebcb2152b9f6679c021268a5ca39e9ab84de
Mathias Nicolajsen <28749155+TidosDK@users.noreply.github.com>
2023-10-11 09:57:05 +0200

    Initial commit of navbar

--- resources/views/pages/frontpage.blade.php
+++ resources/views/pages/frontpage.blade.php
@@ -1,5 +1,5 @@
 @extends('components/layout')
 
 @section('content')
-	<p>Under construction</p>
+	<p class="text-center" style="margin-top: 30px; font-size: 30px">Under construction</p>
 @endsection
---
+++ resources/views/partials/_navigation.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <a class="navbar-brand nav-bar-main-link lilita-one-font" href="/">HAPPY TAILS</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link nav-bar-link lilita-one-font" href="/dogs">DOGS & KITTENS</a>
+				</li>
+                <li class="nav-item active">
+					<a class="nav-link nav-bar-link lilita-one-font" href="/cats">CATS & KITTENS</a>
+				</li>
+                <li class="nav-item active">
+					<a class="nav-link nav-bar-link lilita-one-font" href="/otherpets">OTHER PETS</a>
+				</li>
+                <li class="nav-item active">
+					<a class="nav-link nav-bar-link lilita-one-font" href="aboutus">ABOUT US</a>
+                </li>
+            </ul>
+			<div class="login-button-div">
+				<ul class="navbar-nav">
+					<li>
+						<button type="button" class="btn btn-primary nav-bar-link login-button lilita-one-font"><p class="lilita-one-font mb-0">Login</p></button>
+					</li>
+				</ul>
+			</div>
+        </div>
+    </div>
+</nav>
