99792a18cd257b82cbd358fdbf93a1b1c2818309
Marcus Brink <marct22@student.sdu.dk>
2023-10-15 15:40:27 +0200

    Login and signup views
    Added 2 new .blade.php view files for login and signup.
    Added 1 new .css file with styling for both new view files.
    Created fonts folder as public/css/fonts. Not sure if this is works when setting up a real server instead of localhost
    Added 2 new fonts used by the new .css file
    Added 2 new routes in the web.php file, to enable navigation between login and signup

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages/frontpage');
 });
+
+Route::get('/login', function () {
+    return view('pages/login');
+});
+
+Route::get('/signup', function () {
+    return view('pages/signup');
+});
