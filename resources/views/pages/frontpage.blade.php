1ed99da69e44b32aecb2f03f11f588d4565b5c78
Amaliexiao <amnie22@student.sdu.dk>
2023-10-25 12:00:14 +0200

    Front page view made
    The animals are just as cute as in the other photo!

--- resources/views/pages/frontpage.blade.php
+++ resources/views/pages/frontpage.blade.php
@@ -1,3 +1,21 @@
 <x-layout>
-    <p class="text-center" style="margin-top: 30px; font-size: 30px">Under construction</p>
+
+    <div class="text-center cropped mt-5" style="background-color: #BADEE3">
+        <img src="https://www.americanhumane.org/app/uploads/2016/08/shutterstock_162633491.jpg" class="img-fluid cropped-img"
+            alt="front pic">
+    </div>
+    <h2 class="text-center mt-5">View a selection of all our pets</h2>
+    <hr class="hr-new mt-5">
+    <div class="container text-center mt-5">
+        <div class="row">
+          <div class="col"> <button class="btn view-button btn-lg">View dogs</button>
+          </div>
+          <div class="col">
+            <button class="btn view-button btn-lg">View cats</button>
+          </div>
+          <div class="col">
+            <button class="btn view-button btn-lg">View other pets</button>
+          </div>
+        </div>
+      </div>
 </x-layout>
