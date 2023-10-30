1ff83c273962f32b104ba69bb2226d371f9b2924
VinciDa <marcusbthomsen@gmail.com>
2023-10-30 13:59:09 +0100

    Begining of bootstrapification
    Made most of the login page utilize bootstrap and relative units

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,6 +1,31 @@
 <link rel="stylesheet" href="{{ asset('css/form.css') }}">
 
 <x-layout>
+    <div class="form-container">
+        <h1 class="fs-1 text-center">Happy Tails !</h1>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title text-center">Happy tails make happy tales !</h5>
+
+                <form method="POST" action="/login">
+                    @csrf
+                    <p class="card-text" style="margin-bottom: 0px">Email</p>
+                    <input type="email" class="form-control" name="email">
+                    <p class="card-text" style="margin-bottom: 0px">Password</p>
+                    <input type="password" class="form-control" name="password">
+                    <input type="submit" class="btn btn-primary" value="Log in">
+                </form>
+                <a href="/signup" class="link">Forgot password</a>
+
+                <p class="card-text" style="margin-bottom: 0px">Don't alreadt have an account?</p>
+                <a href="/signup" class="link">Create account</a>
+            </div>
+        </div>
+    </div>
+</x-layout> 
+
+<!--
+<x-layout>
     <h1 class="title-text">Happy Tails !</h1>
     <div class="panel">
         <p class="flair-text">Happy tails make happy tales !</p>
@@ -8,7 +33,6 @@
         <hr class="hr-break">
 
         <form method="POST" action="/login">
-            <!-- Form for submiting login information (Email field, Password field, Login button) -->
             @csrf
             <label for="email" class="prompt-text">Email :</label>
             <input type="email" class="input-field" name="email">
@@ -31,3 +55,4 @@
 
     </div>
 </x-layout>
+
