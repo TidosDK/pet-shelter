958617e4d1013df1a2295e73327927d900c29107
VinciDa <marcusbthomsen@gmail.com>
2023-11-01 11:44:05 +0100

    New font & finalized login/signup view
    - Added new basic font
    - Finalized the login view
    - Finalized the signup view

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -2,57 +2,30 @@
 
 <x-layout>
     <div class="form-container">
-        <h1 class="fs-1 text-center">Happy Tails !</h1>
+        <h1 class="lilita-one-font text-center">Happy Tails !</h1>
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title text-center">Happy tails make happy tales !</h5>
+                <h5 class="lilita-one-font card-title text-center">Happy tails make happy tales !</h5>
+                
+                <hr class="form-hr">
+
+                <h5 class="playpen-bold-font card-text">Log in</h5>
 
                 <form method="POST" action="/login">
                     @csrf
-                    <p class="card-text" style="margin-bottom: 0px">Email</p>
+                    <label for="email" class="playpen-font card-text">Email :</label>
                     <input type="email" class="form-control" name="email">
-                    <p class="card-text" style="margin-bottom: 0px">Password</p>
+                    <label for="password" class="playpen-font card-text">Password :</label>
                     <input type="password" class="form-control" name="password">
                     <input type="submit" class="btn btn-primary" value="Log in">
                 </form>
                 <a href="/signup" class="link">Forgot password</a>
 
-                <p class="card-text" style="margin-bottom: 0px">Don't alreadt have an account?</p>
+                <hr class="form-hr">
+
+                <p class="playpen-font card-text" style="margin-bottom: 0px">Don't already have an account?</p>
                 <a href="/signup" class="link">Create account</a>
             </div>
         </div>
     </div>
 </x-layout> 
-
-<!--
-<x-layout>
-    <h1 class="title-text">Happy Tails !</h1>
-    <div class="panel">
-        <p class="flair-text">Happy tails make happy tales !</p>
-
-        <hr class="hr-break">
-
-        <form method="POST" action="/login">
-            @csrf
-            <label for="email" class="prompt-text">Email :</label>
-            <input type="email" class="input-field" name="email">
-            <label for="password" class="prompt-text">Password :</label>
-            <input type="password" class="input-field" name="password">
-            <div class="submit-holder">
-                <input type="submit" class="submit-button" value="Log in">
-            </div>
-        </form>
-
-        <div class="submit-holder">
-            <a class="submit-button-only-text" href="/forgotPassword">Forgot Password</a>
-        </div>
-        <hr class="hr-break">
-
-        <p class="small-text">Don't already have an account ?</p>
-        <div class="submit-holder">
-            <a class="submit-button-only-text" href="/signup">Create account</a>
-        </div>
-
-    </div>
-</x-layout>
-
--- resources/views/pages/signup.blade.php
+++ resources/views/pages/signup.blade.php
@@ -1,6 +1,46 @@
 <link rel="stylesheet" href="{{ asset('css/form.css') }}">
 
 <x-layout>
+    <div class="form-container">
+        <h1 class="lilita-one-font text-center">Happy Tails !</h1>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="lilita-one-font card-title text-center">Welcome to Happy Tails !</h5>
+                
+                <hr class="form-hr">
+
+                <h5 class="playpen-bold-font card-text">Sign up</h5>
+
+                <form method="POST" action="/signup">
+                    @csrf
+                    <label for="name" class="playpen-font card-text">Display name :</label>
+                    <br>
+                    <input type="text" class="form-control" name="name">
+                    <label for="email" class="playpen-font card-text">Email :</label>
+                    <br>
+                    <input type="email" class="form-control" name="email">
+                    <label for="password" class="playpen-font card-text">Password :</label>
+                    <br>
+                    <input type="password" class="form-control" name="password">
+                    <label for="password_confirmation" class="playpen-font card-text">Repeat password :</label>
+                    <br>
+                    <input type="password" class="form-control" name="password_confirmation">
+                    <div class="submit-holder">
+                        <input type="submit" class="btn btn-primary" value="Sign up">
+                    </div>
+                </form>
+
+                <hr class="form-hr">
+
+                <p class="playpen-font card-text" style="margin-bottom: 0px">Already have an account?</p>
+                <a href="/login" class="link">Log in</a>
+            </div>
+        </div>
+    </div>
+</x-layout> 
+
+<!--
+<x-layout>
     <h1 class="title-text">Happy Tails !</h1>
     <div class="panel">
         <p class="flair-text">Welcome to Happy Tails !</p>
@@ -9,7 +49,6 @@
 
         <form method="POST" action="/signup">
             @csrf
-            <!-- Form for submiting signup information (Display name field, Email field, Password field, Repeat password field, Login button) -->
             <label for="name" class="prompt-text">Display name :</label>
             <input type="text" class="input-field" name="name">
             <label for="email" class="prompt-text">Email :</label>
@@ -32,3 +71,4 @@
 
     </div>
 </x-layout>
+<--
