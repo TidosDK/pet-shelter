0afe78ee628f10b2fc31fb111148cebeed0be616
VinciDa <marcusbthomsen@gmail.com>
2023-11-08 13:40:34 +0100

    Missing feedback label
    Feedback label for missing password on login page has been added

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -17,8 +17,11 @@
                     <label for="email" class="playpen-font card-text">Email :</label>
                     <p class="card-error-text">{{$errors->first('email')}}</p>
                     <input type="email" class="form-control" name="email" value="{{ old('email') }}"> 
+                    
                     <label for="password" class="playpen-font card-text">Password :</label>
+                    <p class="card-error-text">{{$errors->first('password')}}</p>
                     <input type="password" class="form-control" name="password">
+                    
                     <input type="submit" class="btn btn-primary" value="Log in">
                 </form>
                 <a href="/reset-password" class="link">Forgot password</a>
