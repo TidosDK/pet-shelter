d72b8913d9a0ab66c83d7b06f8127b178f5d0eb7
Amaliexiao <amnie22@student.sdu.dk>
2023-11-08 11:40:44 +0100

    small fixes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,12 +5,12 @@ function goToSellerFunction() {
 }
 
 function sendContactMail() {
-    window.scrollTo(0,0);
-    
+    window.scrollTo(0, 0);
+
     let textArea = document.getElementById("message");
     let message = textArea.value;
     textArea.value = "";
-    
+
     let emailInput = document.getElementById("email");
     let email = emailInput.value;
     emailInput.value = "";
@@ -20,7 +20,5 @@ function sendContactMail() {
     alert.innerHTML = '<div class="alert alert-success mt-3" role="alert">Your message has been sent</div>';
     setTimeout(() => {
         alert.style.display = 'none';
-      }, 3000);
-    //console.log(message);
-    //console.log(email);
+    }, 3000);
 }
