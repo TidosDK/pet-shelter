413ab7b1c1338d502ec56770a168f4d61199dec8
Amaliexiao <amnie22@student.sdu.dk>
2023-11-08 14:17:13 +0100

    Textfileds must have input
    Both text inputs must have an input for the 'send messagde' button to have functionallity

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,20 +5,28 @@ function goToSellerFunction() {
 }
 
 function sendContactMail() {
-    window.scrollTo(0, 0);
-
     let textArea = document.getElementById("message");
     let message = textArea.value;
-    textArea.value = "";
 
     let emailInput = document.getElementById("email");
     let email = emailInput.value;
-    emailInput.value = "";
 
-    let alert = document.getElementById('alert-message-area');
-    alert.style.display = 'block';
-    alert.innerHTML = '<div class="alert alert-success mt-3" role="alert">Your message has been sent</div>';
-    setTimeout(() => {
-        alert.style.display = 'none';
-    }, 3000);
+    var inputs = [textArea, emailInput];
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value == '') {
+            return false;
+        }
+        else {
+            window.scrollTo(0, 0);
+            let alert = document.getElementById('alert-message-area');
+            alert.style.display = 'block';
+            alert.innerHTML = '<div class="alert alert-success mt-3" role="alert">Your message has been sent</div>';
+            setTimeout(() => {
+                alert.style.display = 'none';
+            }, 3000);
+            emailInput.value = "";
+            textArea.value = "";
+        }
+    }
 }
