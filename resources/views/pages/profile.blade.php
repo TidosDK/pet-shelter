5448051578d79d378f55834e454b4a18725b2864
Marcus Boel Quebec Jürgensen <54073818+mbqj@users.noreply.github.com>
2023-11-22 15:09:25 +0100

    One form displayed at a time
    only one form can be seen at once

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -2,7 +2,7 @@
 <x-layout :title="$title">
     <h1 class="title-text">{{$title}}</h1>
     
-    <div class="Profile data">
+    <div class="Profile data" id="dataForm">
         <form>
             <label>Display name: </label>
             <label>{{{auth()->user()->name}}}</label><br>
@@ -12,7 +12,7 @@
         </form>
     </div>
 
-    <div class="Edit profile">
+    <div class="Edit profile" id="editForm" style="display: none">
         <form>
             <label>Display name: </label>
             <input type="text" value={{{auth()->user()->name}}}><br>
@@ -22,5 +22,26 @@
         </form>
     </div>
     
+    <script>
+        function edit(){
+            var displayDiv = document.getElementById("dataForm")
+            var editDiv = document.getElementById("editForm");
+            var editBut = document.getElementById("edit button");
+            
+
+            if(editDiv.style.display === "none"){
+                editDiv.style.display = "block";
+                displayDiv.style.display = "none";
+                editBut.innerHTML = "save";
+            }
+            else{
+                editDiv.style.display = "none";
+                displayDiv.style.display = "block";
+                editBut.innerHTML = "edit";
+            }
+        }
+    </script>
     <a class="btn view-button btn-lg" href="/post-management">View my posts</a>
+    <a class="btn view-button btn-lg" id="edit button" onclick="edit()">Edit</a>
+    
 </x-layout>
