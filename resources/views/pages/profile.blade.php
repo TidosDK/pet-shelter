f31c3550d2d2f66fe8cc8c64f1e8b823f4537171
TidosDK <28749155+TidosDK@users.noreply.github.com>
2023-11-29 13:49:24 +0100

    Formattet and fixed too many brackets in references

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,39 +1,43 @@
-<?php $title = "Profile" ?>
+<?php $title = 'Profile'; ?>
 
 <script src="{{ asset('js\profileScripts.js') }}"></script>
 
 <x-layout :title="$title">
-    <h1 class="lilita-one-font">{{$title}}</h1>
-    
+    <h1 class="lilita-one-font mt-4 mb-3">{{ $title }}</h1>
+
     <div class="Profile data lilita-one-font" id="dataForm">
         <form method="POST" action="/profile">
             @csrf
-            <p class="mb-0 color-navy">{{$errors->first('name')}}</p>
+            <p class="mb-0 color-navy">{{ $errors->first('name') }}</p>
             <label class="color-blue">Display name: </label>
-            <label>{{{auth()->user()->name}}}</label><br>
+            <label>{{ auth()->user()->name }}</label><br>
             <br>
-            <p class="mb-0 color-navy"">{{$errors->first('email')}}</p>
+            <p class="mb-0 color-navy"">{{ $errors->first('email') }}</p>
             <label class="color-blue">E-mail: </label>
-            <label>{{{auth()->user()->email}}} </label><br>
+            <label>{{ auth()->user()->email }} </label><br>
             <br>
-            <p class="mb-0 color-navy"">{{$errors->first('phone')}}</p>
+            <p class="mb-0 color-navy"">{{ $errors->first('phone') }}</p>
             <label class="color-blue">Phone number: </label>
-            <label>{{{auth()->user()->phone}}} </label><br>
-            <a class="btn view-button btn-lg" id="viewPost" href="/post-management">View my posts</a>
-            <button class="btn view-button btn-lg" type="button" id="editBtn">Edit</button>
+            <label>{{ auth()->user()->phone }} </label><br>
+            <div class="mt-4">
+                <a class="btn view-button btn-lg" id="viewPost" href="/post-management">View my posts</a>
+                <button class="btn view-button btn-lg" type="button" id="editBtn">Edit</button>
+            </div>
     </div>
     <div class="Edit profile lilita-one-font" id="editForm" style="display: none">
-            
-            <label class="color-blue">Display name: </label>
-            <input type="text" name="name" value={{{auth()->user()->name}}}><br>
-            <br>
-            <label class="color-blue">E-mail: </label>
-            <input type="text" name="email" value={{{auth()->user()->email}}}><br>
-            <br>
-            <label class="color-blue">Phone number: </label>
-            <input type="text" name="phone" value={{{auth()->user()->phone}}}><br>
+
+        <label class="color-blue">Display name: </label>
+        <input type="text" name="name" value={{ auth()->user()->name }}><br>
+        <br>
+        <label class="color-blue">E-mail: </label>
+        <input type="text" name="email" value={{ auth()->user()->email }}><br>
+        <br>
+        <label class="color-blue">Phone number: </label>
+        <input type="text" name="phone" value={{ auth()->user()->phone }}><br>
+        <div class="mt-4">
             <a class="btn view-button btn-lg" href="/post-management">View my posts</a>
-            <input class="btn view-button btn-lg" type="submit" value="save">
+            <input class="btn view-button btn-lg ml-2" type="submit" value="save">
+        </div>
         </form>
     </div>
 </x-layout>
