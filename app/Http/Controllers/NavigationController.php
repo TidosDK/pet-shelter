1ca158c2eeb0facd9a71f6a38df5d17742452787
TidosDK <28749155+TidosDK@users.noreply.github.com>
2023-12-21 17:42:05 +0100

    Revert "Backend and extra frontend components"
    This reverts commit 3e2a2b98429b86dc5d9e9494bcb42da8c9ee3f1c.
    
    # Conflicts:
    #	app/Http/Controllers/NavigationController.php

--- app/Http/Controllers/NavigationController.php
+++ app/Http/Controllers/NavigationController.php
@@ -5,9 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Pets;
 use App\Models\Breeds;
 use App\Models\TypesOfPets;
-use App\Models\UserLikesPet;
-use App\Http\Controllers\ReactionController;
-use App\Models\TypesOfLikes;
 
 class NavigationController extends Controller {
 
@@ -33,17 +30,8 @@ class NavigationController extends Controller {
 	}
 
 	public function singlePetView(string $pet_id) {
-		//Get the amount of each type of reaction for this pet
-		$reaction_counts = ReactionController::petReactionCountsAll($pet_id);
-
-		//Get the authenticated users current reaction to this pet
-		$current_reaction = ReactionController::currentReaction($pet_id);
-
 		return view('pages.pet', [
-			'pet' => Pets::find($pet_id),
-			'reaction_types' => TypesOfLikes::all(),
-			'reactions' => $reaction_counts,
-			'current_reaction' => $current_reaction
+			'pet' => Pets::find($pet_id)
 		]);
 	}
 }
