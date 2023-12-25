087c90988bf173139b609b95e626bf08f6f10cb4
TidosDK <28749155+TidosDK@users.noreply.github.com>
2023-12-25 03:15:19 +0100

    Moved NavigationController tests into unit test file

--- tests/Feature/NavigationControllerTest.php
+++ tests/Feature/NavigationControllerTest.php
@@ -8,41 +8,6 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class NavigationControllerTest extends TestCase {
-    public function testFrontPageView() {
-        $this->get('/')
-            ->assertStatus(200)
-            ->assertSee('frontpage-image');
-    }
-
-    public function testAboutUsView() {
-        $this->get('/aboutUs')
-            ->assertStatus(200)
-            ->assertSee('aboutus-image');
-    }
-
-    public function testPetsView() {
-        $this->get('/pets/dogs')
-            ->assertSessionHasNoErrors()
-            ->assertStatus(200)
-            ->assertSee('sorting-settings-for-dogs')
-            ->assertDontSee('sorting-settings-for-cats');
-
-        $this->get('/pets/cats')
-            ->assertSessionHasNoErrors()
-            ->assertStatus(200)
-            ->assertSee('sorting-settings-for-cats')
-            ->assertDontSee('sorting-settings-for-dogs');
-    }
-
-    public function testOtherPetsView() {
-        $this->get('/pets')
-            ->assertSessionHasNoErrors()
-            ->assertStatus(200)
-            ->assertSee('sorting-settings-for-')
-            ->assertDontSee('sorting-settings-for-dogs')
-            ->assertDontSee('sorting-settings-for-cats');
-    }
-
     public function testSinglePetView() {
         $petId = PostControllerTest::getTestPet()->id;
 
--- tests/Unit/NavigationControllerTest.php
+++ tests/Unit/NavigationControllerTest.php
@@ -6,7 +6,38 @@ use Tests\TestCase;
 use Tests\Unit\PostControllerTest;
 
 class NavigationControllerTest extends TestCase {
-    public function testExample() {
-        $this->assertTrue(true);
+    public function testFrontPageView() {
+        $this->get('/')
+            ->assertStatus(200)
+            ->assertSee('frontpage-image');
+    }
+
+    public function testAboutUsView() {
+        $this->get('/aboutUs')
+            ->assertStatus(200)
+            ->assertSee('aboutus-image');
+    }
+
+    public function testPetsView() {
+        $this->get('/pets/dogs')
+            ->assertSessionHasNoErrors()
+            ->assertStatus(200)
+            ->assertSee('sorting-settings-for-dogs')
+            ->assertDontSee('sorting-settings-for-cats');
+
+        $this->get('/pets/cats')
+            ->assertSessionHasNoErrors()
+            ->assertStatus(200)
+            ->assertSee('sorting-settings-for-cats')
+            ->assertDontSee('sorting-settings-for-dogs');
+    }
+
+    public function testOtherPetsView() {
+        $this->get('/pets')
+            ->assertSessionHasNoErrors()
+            ->assertStatus(200)
+            ->assertSee('sorting-settings-for-')
+            ->assertDontSee('sorting-settings-for-dogs')
+            ->assertDontSee('sorting-settings-for-cats');
     }
 }
